08524083579a8f4dde8ae20479bbbe8a97dbea27
Umang A <umanga.eng@gmail.com>
2025-07-14 23:33:24 -0400

    config changes

--- config/gcs.js
+++ config/gcs.js
@@ -1,19 +1,15 @@
 const { Storage } = require('@google-cloud/storage');
-const path = require('path');
-require('dotenv').config();
 
-const projectId = process.env.GOOGLE_CLOUD_PROJECT_ID;
-const bucketName = process.env.GOOGLE_CLOUD_STORAGE_BUCKET;
+const serviceAccount = {
+  type: 'service_account',
+  project_id: process.env.GCP_PROJECT_ID,
+  private_key_id: process.env.GCP_PRIVATE_KEY_ID,
+  private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n'), // fix line breaks
+  client_email: process.env.GCP_CLIENT_EMAIL,
+  client_id: process.env.GCP_CLIENT_ID,
+};
 
-if (!projectId || !bucketName) {
-  throw new Error('Missing required environment variables for GCS.');
-}
-
-const storage = new Storage({
-  projectId,
-  keyFilename: path.join(__dirname, './serviceAccountKey.json'), // keep in /config/
-});
-
-const bucket = storage.bucket(bucketName);
+const storage = new Storage({ credentials: serviceAccount });
+const bucket = storage.bucket(process.env.GOOGLE_CLOUD_STORAGE_BUCKET);
 
 module.exports = { storage, bucket };
