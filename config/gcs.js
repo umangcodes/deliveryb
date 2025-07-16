336e6adc37da8d43e04e97bd6b3fd7dc1fdf789a
Umang A <umanga.eng@gmail.com>
2025-07-16 08:50:50 -0400

    var updates

--- config/gcs.js
+++ config/gcs.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const projectId = process.env.GOOGLE_CLOUD_PROJECT_ID;
 const bucketName = process.env.GOOGLE_CLOUD_STORAGE_BUCKET;
 
-const privateKey = process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+const privateKey = process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
-if (!projectId || !bucketName || !privateKey || !process.env.GOOGLE_CLIENT_EMAIL) {
+if (!projectId || !bucketName || !privateKey || !process.env.GCP_CLIENT_EMAIL) {
   throw new Error('Missing required Google Cloud env variables.');
 }
 
