36a5f3117810e56d804b7635f78104010949c5a2
Umang A <umanga.eng@gmail.com>
2026-02-17 20:20:00 -0500

    cors fix

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],// only matters if you use cookies; safe to leave true if you do
 }));
-app.options("*", cors());
+app.options("/*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
